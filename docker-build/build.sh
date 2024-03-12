62d859fe67ba304350919c651fa3d42725953e7d
stkptr <85911392+stkptr@users.noreply.github.com>
2024-03-12 14:38:56 -0700

    Improve Android buildsystem, provide start to finish builds The build script can now build the extension and export the demo project.

--- docker-build/build.sh
+++ docker-build/build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-DOCKER_BUILDKIT=1 docker build \
-    --output=../bin/android --target=binaries -f Dockerfile.android ..
+export DOCKER_BUILDKIT=1
+
+docker build -t androidsdk:latest -f Dockerfile.androidsdk .
+docker build -t androidgd:latest -f Dockerfile.androidgd .
+
+docker build \
+    --output=../bin/android --target=binaries -f Dockerfile.androidbuild ..
+
+cp ../bin/android/* ../demo/bin/android
+
+docker build \
+    --output=../builds --target=binaries -f Dockerfile.androidexport ..
