7627f4f16a2800ecbc128d4e786ec04e1bd802e6
stkptr <85911392+stkptr@users.noreply.github.com>
2024-03-12 18:31:40 -0700

    Refactor Docker buildsystem This should serve as a good base for building binaries for platforms other than Android.

--- docker-build/build.sh
+++ docker-build/build.sh
@@ -1,14 +1,35 @@
 #!/bin/sh
 
+GDBASE=https://github.com/godotengine/godot/releases/download/
+TEMPLATES=4.2.1-stable/Godot_v4.2.1-stable_export_templates.tpz
+
+if [ ! -f templates.zip ]; then
+    wget -O templates.zip "${GDBASE}/${TEMPLATES}"
+fi
+
+if [ ! -d templates ]; then
+    unzip templates.zip
+fi
+
 export DOCKER_BUILDKIT=1
 
-docker build -t androidsdk:latest -f Dockerfile.androidsdk .
-docker build -t androidgd:latest -f Dockerfile.androidgd .
+docker_build() {
+    docker build -t ${1}:latest -f ${1}.docker .
+}
 
-docker build \
-    --output=../bin/android --target=binaries -f Dockerfile.androidbuild ..
+gdbuild() {
+    docker build \
+        --output=../bin/${1} --target=binaries -f ${1}-build.docker ..
+    cp ../bin/${1}/* ../demo/bin/${1}
+}
 
-cp ../bin/android/* ../demo/bin/android
+gdexport() {
+    docker build \
+        --output=../builds/${1} --target=binaries -f ${1}-export.docker ..
+}
 
-docker build \
-    --output=../builds --target=binaries -f Dockerfile.androidexport ..
+docker_build debgd
+
+docker_build androidgd
+gdbuild android
+gdexport android
